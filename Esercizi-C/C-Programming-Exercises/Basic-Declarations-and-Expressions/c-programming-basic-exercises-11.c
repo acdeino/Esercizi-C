ddf8b9c1ebc55da1169eca6c9aeb0d152890e0c4
namlose <ahbehnomics@gmail.com>
2018-03-04 01:48:43 +0100

    Write a C program that accepts two item’s weight (floating points' values ) and number of purchase (floating points' values) and calculate the average value of the items. Go to the editor Test Data : Weight - Item1: 15 No. of item1: 5 Weight - Item2: 25 No. of item2: 4

---
+++ Esercizi-C/C-Programming-Exercises/Basic-Declarations-and-Expressions/c-programming-basic-exercises-11.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main (void) {
+
+int types;
+int weight1;
+int nmbritem1;
+int weight2;
+int nmbritem2;
+
+start:
+
+  printf("Please insert here how many types of items you have gotten:\n");
+  scanf("%d", &types);
+
+  if (types > 2){
+    printf("Please insert a number be greater than 1, but no greater than 2.\n");
+    goto start;
+  }
+  else
+  if ( types <= 1 ){
+    printf("The number of the items measured must be greater than 1, but no greater than 2.\n");
+    goto start;
+  }
+  else
+  if ( types == 2 ){
+  printf("Insert here how many pieces do you have of the first type of Item:");
+  scanf("%d\n", nmbritem1);
+  printf("Now insert here the weight of type one item\n");
+  scanf("%d\n", weight1);
+  printf("Please insert here the number of type 2 items\n");
+  scanf("%d\n", nmbritem2);
+  printf("One more thing! How much do type 2 items weight? Please insert the value here:\n");
+  scanf("%d\n", weight2);
+
+int totalweight = (weight1*nmbritem1)+(weight2*nmbritem2);
+int totalnumbers = nmbritem1+nmbritem2;
+float avgweight = totalweight / totalnumbers;
+
+averageweight:
+printf("The average weight of the items is %f\n", avgweight);
+}
+  return 0;
+}
