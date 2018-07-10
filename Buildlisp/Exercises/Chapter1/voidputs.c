d2a794bc1020b72a106061cdd3e02961acd30850
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-10 21:15:41 +0200

    -

--- Buildlisp/Exercises/Chapter1/voidputs.c
+++ Buildlisp/Exercises/Chapter1/voidputs.c
@@ -5,3 +5,9 @@
 
   return 0;
 }
+
+
+// puts                  Write string to stdout (function )
+// printf                Print formatted data to stdout (function)
+
+//gdb                    is simply the GNU debugger which can be used with C
