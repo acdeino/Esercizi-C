da3b316e6a856d90a22a5bbc836006edc2a01913
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-10 23:04:24 +0200

    -

---
+++ Buildlisp/Exercises/Chapter2/fivetimesforloop.c
@@ -0,0 +1,8 @@
+#include <STDIO.h>
+
+int main () {
+  for (int i = 0; i < 5; i++) {
+    puts(" Hello World !\n");
+  }
+return 0;
+}
---
+++ Buildlisp/Exercises/Chapter2/helloworldfrommain.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main() {
+
+  void sayhello (int n)
+  {
+    int i;
+    for (i = 1; i <= n; i++){
+      puts("Hello World!\n");
+    }
+  }
+
+return 0;
+}
+
+
+// signed char, long int, signed long, signed long int, etc etc...
+
+// !=, ==
+
+// division /, multiplication *
+
+//a += b ->	a = a + b
+
+//  Unlike for and while loops, which test the loop condition at the top of the loop, the do...while loop in C programming checks its condition at the bottom of the loop. A do...while loop is similar to a while loop, except the fact that it is guaranteed to execute at least one time.
+/* do {
+   statement(s);
+} while( condition ); */
+
+//A switch statement allows a variable to be tested for equality against a list of values. Each value is called a case, and the variable being switched on is checked for each switch case.
+
+// break; statement breaks the inner loop it is written into, interrupts command. Can be used for switch statement as well
+
+// continue; is kinda like break; instead of interrupting the loop, it forces the next iteration of the loop to take place, skipping any code in between
+
+
+// typedef allows to explictly define new names for data types
