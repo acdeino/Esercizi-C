c004b8228d19563738012a227ee9bd4704c07bd0
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-23 21:02:25 +0200

    .

--- C-Begin/helloworld3.c
+++ C-Begin/helloworld3.c
@@ -4,7 +4,14 @@
 // int main  (num of args parsed in main, sequential list of strings)
 int main(int argc, char **argv) {
   printf("Hello World!\n");
-  printf("3");
+  printf("3\n");
+  printf("\n");
+  printf("\n");
+
+  int i;
+  for (i = 0; i < argc; i++) {
+    printf("Hello World: argc=%d arg %d is %s\n", argc, i, argv[i]);
+  }
 
   return 0;
 }
