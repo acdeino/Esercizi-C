e998cdbaddd2a0fc7ef904e54654d6e600293020
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-08-24 21:05:13 +0200

    .

--- C-Begin/compoundoperators.c
+++ C-Begin/compoundoperators.c
@@ -10,7 +10,7 @@ void showPrefixandPostfixOps ()
   int num2;
   printf("\nPrefix and Postfix operators. . . (num = 10)");
   num1 = 10;
-  num2 = num1++; // num2 = 11; num1 = 11
+  num2 = num1++; // num2 = 10; num1 = 11
   printf("\nnum2 = ++num1; so num2 = %d and num1 = %d", num2, num1);
 
   num1 = 10;
--- C-Begin/ifelse.c
+++ C-Begin/ifelse.c
@@ -11,15 +11,20 @@ int main (int argc, char **argv) {
   gets(agestring);
   age = atoi(agestring);
 
+  if (age == 0) {
+    printf("You entered an invalid age, so your bonus can't be calculated.\n");
+  } else {
   if (age > 45) {
     bonus = 1000;
+  } else if (age >= 1) {
+    bonus = 500;
   } else {
     bonus = 500;
   }
-
+}
   printf("Your age is %d, so your bonus is %d\n", age, bonus);
-
-  if (age <= 21) {
+   {
+  if (age <= 21 && age >= 1) {
     printf("You are one of our youngest employees!\n");
   } else if (age >= 70) {
     printf("Is time to retire, innit?\n");
@@ -27,7 +32,7 @@ int main (int argc, char **argv) {
   else {
     printf("You're average.\n");
   }
-
+}
   if (bonus >= 1000) {
     printf("You've earned a high bonus!\n");
   } else {
