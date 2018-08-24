8f9389f4164986ef6b096604af1cdfc04e6acede
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-08-24 22:19:58 +0200

    .

---
+++ C-Begin/logicaloperators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+  printf("These are the logical operators: \n");
+  printf("\n");
+  printf("\n");
+  printf("&& means AND\n");
+  printf("|| means OR\n");
+
+  return 0;
+}
