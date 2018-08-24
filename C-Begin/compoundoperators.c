836b1a912c79027f0bf8dafdc23bccf52adf4020
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-08-24 20:51:29 +0200

    .

--- C-Begin/compoundoperators.c
+++ C-Begin/compoundoperators.c
@@ -3,6 +3,21 @@
 int a;
 int b;
 
+
+void showPrefixandPostfixOps ()
+{
+  int num1;
+  int num2;
+  printf("\nPrefix and Postfix operators. . . (num = 10)");
+  num1 = 10;
+  num2 = num1++; // num2 = 11; num1 = 11
+  printf("\nnum2 = ++num1; so num2 = %d and num1 = %d", num2, num1);
+
+  num1 = 10;
+  num2 = --num1;
+  printf("\nnum2 = --num1; so num2 = %d and num1 = %d", num2, num1);
+}
+
 int main ()
 {
   a = 10;
@@ -41,6 +56,10 @@ int main ()
   printf("Prefix & Postfix operators... (num = 10)\n");
   printf("num2 = num1++; so, num2 = 10 & num1 = 11\n");
   printf("num2 = ++num1; so, num2 = 11 & num1 = 11\n");
+  printf("\n");
+  printf("\n");
+
+  showPrefixandPostfixOps();
 
   return 0;
 }
